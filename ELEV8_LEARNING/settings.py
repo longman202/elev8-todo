eec6193ee2d9faf69d6328a1286a8463a4ccc25b
Longman <opesco202@gmail.com>
2022-06-13 00:57:32 +0100

    adding whitenoise settings

--- ELEV8_LEARNING/settings.py
+++ ELEV8_LEARNING/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!r_4_0+#jr)6zx%uemz9x*h&4*u$al(578#w=(%vf=-8a!(4=+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['longman-todo.azurewebsites.net']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = ['static']
 
 # Default primary key field type
